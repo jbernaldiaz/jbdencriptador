3f1c63309ec86bfcdb2675f6ad88b42a800a2b65
jbernaldiaz <jbernaldiaz@gmail.com>
2023-05-29 23:06:08 -0300

    Listo funcionando

--- logica.js
+++ logica.js
@@ -1,22 +1,35 @@
-let str = document.querySelector(".text-area");
+const string = document.querySelector(".text-area");
 let mensaje = document.querySelector(".mensaje");
 
- function btnClick(){
-    let strEncriptado = encriptador(str.value);
+ function btnEncriptar(){
+    let strEncriptado = encriptador(string.value);
     mensaje.value = strEncriptado;
     mensaje.style.backgroundImage = "none"
-    textArea.value = "";
+    string.value = "";
 }
+
+function btnDesencriptar(){
+    let strEncriptado = desencriptador(string.value);
+    mensaje.value = strEncriptado;
+    mensaje.style.backgroundImage = "none"
+    string.value = "";
+}
+
+
+
+
 
 function encriptador(texto){
 
-   const encriptacion = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]]; 
+   const encriptacion = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+   
+   texto = texto.toLowerCase()
     
-    for(let i=0;i<encriptacion.length;i++){
+    for(let i=0; i<encriptacion.length; i++){
 
-       
+       let letra = encriptacion[i][0];
 
-        if(text.includes(encriptacion[i][0])){
+        if(texto.includes(letra)){
             texto = texto.replaceAll(letra, encriptacion[i][1]);
         }
     }
@@ -24,4 +37,25 @@ function encriptador(texto){
     return texto;
 }
 
+function desencriptador(texto){
+   const encriptacion = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
 
+   for(let i=0; i<encriptacion.length; i++){
+
+    let letra = encriptacion[i][1];
+
+     if(texto.includes(letra)){
+         texto = texto.replaceAll(letra, encriptacion[i][0]);
+     }
+ }
+
+ return texto;
+}
+
+
+function btnCopiar(){
+    let strEncriptado = mensaje.value;
+    string.value = strEncriptado;
+    //mensaje.style.backgroundImage = 
+    mensaje.value = "";
+}
